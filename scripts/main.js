a4d790f536fa257aa119b76ef2c2a6f5c4f2b638
Paradine181 <paradine181@gmail.com>
2017-09-25 16:56:11 +0300

    script

--- scripts/main.js
+++ scripts/main.js
@@ -27,12 +27,13 @@ function Model(name) {
     };
 }
 
-function Chapter(model, name) {
+function Chapter(model, name, direction) {
     this.name = name;
+    this.direction = direction;
     this.paragraphs = [];
     this.images = [];
-    this.addParagraph = function(text) {
-        this.paragraphs.push(text);
+    this.addParagraph = function(text, type) {
+        this.paragraphs.push({type: type, content: text});
     };
     this.addImage = function(url, description) {
         this.images.push(new Image(url, description));
@@ -48,13 +49,15 @@ function Image(url, description) {
 window.addEventListener('load', function() {
     loadDeferredStyles();
     initializeModels();
+    toggleModalAnimation();
 
     var galleryModal = document.querySelector('.gallery-modal');
     var galleryModalClose = galleryModal.querySelector('.close');
 
     galleryModalClose.addEventListener('click', function() {
-        galleryModal.style.display = 'none';
-        galleryModal.style.visibility = 'hidden';
+        if (galleryModal.classList.contains('is-paused')){
+            galleryModal.classList.remove('is-paused');
+        }
     });
 });
 
@@ -69,13 +72,13 @@ function loadDeferredStyles() {
 
 function setModelView(model) {
     var main = document.querySelector('main');
-    var header = main.querySelector('.model-header')
+    var header = main.querySelector('header')
     var headerTitle = document.createElement('h2');
 
     clearModelView();
 
     headerTitle.innerHTML = model.name;
-    headerTitle.setAttribute('class', 'model-title');
+    headerTitle.setAttribute('class', 'ltcs-item-grow');
 
     header.appendChild(headerTitle);
 
@@ -117,9 +120,10 @@ function setChapterView(chapter) {
     clearChapterView();
     
     modelHeader.innerHTML = chapter.name;
+    modelText.dir = chapter.direction;
 
     for (var i = 0; i < chapter.paragraphs.length; i++) {
-        addParagraphToChapter(chapter.paragraphs[i]);
+        addParagraphToChapter(chapter.paragraphs[i].content, chapter.paragraphs[i].type);
     }
 
     for (var i = 0; i < chapter.images.length; i++) {
@@ -148,18 +152,47 @@ function clearChapterView() {
     }
 }
 
-function addParagraphToChapter(paragraphUrl) {
+function addParagraphToChapter(paragraphUrl, type) {
     var chapterContent = document.querySelector('.model-content-text');
     var p = document.createElement('p');
     var newLine = document.createElement('br');
 
     getFile(paragraphUrl, function(paragraph) {
-        const paragraphHTML = (require('highlight-nl')(paragraph)).replace(/\r\n/g, "\n").replace(/\n/g, '<br />').replace(/\t/g, '&nbsp;&nbsp;&nbsp;&nbsp;').replace(/  /g, '&nbsp;&nbsp;&nbsp;&nbsp;');
-        p.innerHTML = paragraphHTML;
+        if (type === 'code') {
+            const paragraphHTML = (require('highlight-nl')(paragraph)).replace(/\r\n/g, "\n").replace(/\n/g, '<br />').replace(/\t/g, '&nbsp;&nbsp;&nbsp;&nbsp;').replace(/  /g, '&nbsp;&nbsp;&nbsp;&nbsp;');
+            p.innerHTML = paragraphHTML;
+        } else {
+            const paragraphHTML = paragraph;
+            p.innerHTML = paragraphHTML;
+        }
     });
 
     chapterContent.appendChild(p);
     chapterContent.appendChild(newLine);
+}
+
+function toggleModalAnimation() {
+    var galleryModal = document.querySelector('.gallery-modal');
+    
+    galleryModal.addEventListener('animationend', function() {
+        if (!galleryModal.classList.contains('is-paused')){
+            galleryModal.classList.add('is-paused');
+        }
+
+        if (galleryModal.classList.contains('fade-out')) {
+            if (!galleryModal.classList.contains('fade-in'))
+                galleryModal.classList.add('fade-in');
+            galleryModal.classList.remove('fade-out');
+            galleryModal.style.display = 'none';
+            galleryModal.style.visibility = 'hidden';
+        } else if (galleryModal.classList.contains('fade-in')) {
+            if (!galleryModal.classList.contains('fade-out'))
+                galleryModal.classList.add('fade-out');
+            galleryModal.classList.remove('fade-in');
+        } else {
+            galleryModal.classList.add('fade-in');
+        }
+    });
 }
 
 function addImageToGallery(image) {
@@ -181,6 +214,10 @@ function addImageToGallery(image) {
         galleryModalImage.src = this.src;
         galleryModalImage.alt = this.alt;
         galleryModalCaption.innerHTML = this.alt;
+
+        if (galleryModal.classList.contains('is-paused')){
+            galleryModal.classList.remove('is-paused');
+        }
     });
 
     galleryDescription.setAttribute('class', 'gallery-caption');
@@ -211,128 +248,166 @@ function getFile(path, callback) {
 
 function initializeModels() {
     /* Introduction */
-    var introModel = new Model('Introduction');
-    var introModelAccessibility = new Chapter(introModel, 'Accessibility');
-    var introModelSonification = new Chapter(introModel, 'Sonification');
-    var introModelNetLogo = new Chapter(introModel, 'NetLogo');
+    var introModel = new Model('הקדמה');
+    var introModelAccessibility = new Chapter(introModel, 'מבוא', 'rtl');
+    var introModelSonification = new Chapter(introModel, 'סוניפיקציה', 'rtl');
+
+    introModelAccessibility.addParagraph('./resources/models/Introduction/About01.html', 'text');
+
+    introModelSonification.addParagraph('./resources/models/Introduction/About02.html', 'text');
+    
     document.querySelector('#Introduction').addEventListener('click', () => { setModelView(introModel); })
     
-    /* 1. Neils */
-    var neilsModel = new Model('Neils - Current in a Wire');
-    var neilsModelAbout = new Chapter(neilsModel, 'About the Model');
-    var neilsModelCode = new Chapter(neilsModel, 'Source Code');
+    /* 1. Niels */
+    var neilsModel = new Model('מודל החשמל - נילס');
+    var neilsModelAbout = new Chapter(neilsModel, 'אודות המודל', 'rtl');
+    var neilsModelCode = new Chapter(neilsModel, 'קוד המודל', 'ltr');
 
-    neilsModelAbout.addImage('./images/models/Neils/NeilsParallel_0.png', 'Neils - Initial State');
-    neilsModelAbout.addImage('./images/models/Neils/NeilsParallel_1.png', 'Neils - Parallel Circuit');
-    neilsModelAbout.addImage('./images/models/Neils/NeilsSerial_1.png', 'Neils - Serial Circuit');
-    neilsModelAbout.addImage('./images/models/Neils/NeilsSingle_1.png', 'Neils - Single Wire');
+    neilsModelAbout.addImage('./resources/models/Neils/Neils_0.png', 'בקרי סוניפיקציה');
+    neilsModelAbout.addImage('./resources/models/Neils/NeilsParallel_1.png', 'מצב התחלתי');
+    neilsModelAbout.addImage('./resources/models/Neils/NeilsParallel_2.png', 'מעגל מקבילי');
+    neilsModelAbout.addImage('./resources/models/Neils/NeilsSerial_2.png', 'מעגל טורי');
+    neilsModelAbout.addImage('./resources/models/Neils/NeilsSingle_2.png', 'מוליך יחיד');
 
-    neilsModelCode.addParagraph('./images/models/Neils/SourceCode.txt');
+    neilsModelAbout.addParagraph('./resources/models/Neils/About.html', 'text');
+
+    neilsModelCode.addParagraph('./resources/models/Neils/SourceCode.txt', 'code');
 
     document.querySelector('#Neils').addEventListener('click', () => { setModelView(neilsModel); })
     
     /* 2. GasLab */
-    var gasLabModel = new Model('Gas Lab');
-    var gasLabModelAbout = new Chapter(gasLabModel, 'About the Model');
-    var gasLabModelCode = new Chapter(gasLabModel, 'Source Code');
+    var gasLabModel = new Model('מודל הגז');
+    var gasLabModelAbout = new Chapter(gasLabModel, 'אודות המודל', 'rtl');
+    var gasLabModelCode = new Chapter(gasLabModel, 'קוד המודל', 'ltr');
 
-    gasLabModelAbout.addImage('./images/models/GasLab/GasLab_0.png', 'Gas Lab - Initial State');
-    gasLabModelAbout.addImage('./images/models/GasLab/GasLab_1.png', 'Gas Lab');
+    gasLabModelAbout.addImage('./resources/models/GasLab/GasLab_0.png', 'בקרי סוניפיקציה');
+    gasLabModelAbout.addImage('./resources/models/GasLab/GasLab_1.png', 'מצב התחלתי');
+    gasLabModelAbout.addImage('./resources/models/GasLab/GasLab_2.png', 'מודל הגז');
 
-    gasLabModelCode.addParagraph('./images/models/GasLab/SourceCode.txt');
+    gasLabModelAbout.addParagraph('./resources/models/GasLab/About.html', 'text');
+    
+    gasLabModelCode.addParagraph('./resources/models/GasLab/SourceCode.txt', 'code');
 
     document.querySelector('#GasLab').addEventListener('click', () => { setModelView(gasLabModel); })
     
     /* 3. GasLab - Moving Piston */
-    var gasLabMovingPistonModel = new Model('Gas Lab - Moving Piston');
-    var gasLabMovingPistonModelAbout = new Chapter(gasLabMovingPistonModel, 'About the Model');
-    var gasLabMovingPistonModelCode = new Chapter(gasLabMovingPistonModel, 'Source Code');
+    var gasLabMovingPistonModel = new Model('מודל הגז עם בוכנה נעה');
+    var gasLabMovingPistonModelAbout = new Chapter(gasLabMovingPistonModel, 'אודות המודל', 'rtl');
+    var gasLabMovingPistonModelCode = new Chapter(gasLabMovingPistonModel, 'קוד המודל', 'ltr');
 
-    gasLabMovingPistonModelAbout.addImage('./images/models/GasLabMovingPiston/GasLabMovingPiston_0.png', 'Gas Lab Moving Piston - Initial State');
-    gasLabMovingPistonModelAbout.addImage('./images/models/GasLabMovingPiston/GasLabMovingPiston_1.png', 'Gas Lab Moving Piston');
+    gasLabMovingPistonModelAbout.addImage('./resources/models/GasLabMovingPiston/GasLabMovingPiston_0.png', 'בקרי סוניפיקציה');
+    gasLabMovingPistonModelAbout.addImage('./resources/models/GasLabMovingPiston/GasLabMovingPiston_1.png', 'מצב התחלתי');
+    gasLabMovingPistonModelAbout.addImage('./resources/models/GasLabMovingPiston/GasLabMovingPiston_2.png', 'מודל הגז עם בוכנה נעה');
 
-    gasLabMovingPistonModelCode.addParagraph('./images/models/GasLabMovingPiston/SourceCode.txt');
+    gasLabMovingPistonModelAbout.addParagraph('./resources/models/GasLabMovingPiston/About.html', 'text');
+
+    gasLabMovingPistonModelCode.addParagraph('./resources/models/GasLabMovingPiston/SourceCode.txt', 'code');
 
     document.querySelector('#GasLabMovingPiston').addEventListener('click', () => { setModelView(gasLabMovingPistonModel); })
     
     /* 4. Ising */
-    var isingModel = new Model('Ising');
-    var isingModelAbout = new Chapter(isingModel, 'About the Model');
-    var isingModelCode = new Chapter(isingModel, 'Source Code');
+    var isingModel = new Model('מודל Ising');
+    var isingModelAbout = new Chapter(isingModel, 'אודות המודל', 'rtl');
+    var isingModelCode = new Chapter(isingModel, 'קוד המודל', 'ltr');
 
-    isingModelAbout.addImage('./images/models/Ising/Ising_0.png', 'Ising - Initial State');
-    isingModelAbout.addImage('./images/models/Ising/Ising_1.png', 'Ising');
+    isingModelAbout.addImage('./resources/models/Ising/Ising_0.png', 'בוררי סוניפיקציה');
+    isingModelAbout.addImage('./resources/models/Ising/Ising_1.png', 'מצב התחלתי');
+    isingModelAbout.addImage('./resources/models/Ising/Ising_2.png', 'מודל Ising');
 
-    isingModelCode.addParagraph('./images/models/Ising/SourceCode.txt');
+    isingModelAbout.addParagraph('./resources/models/Ising/About.html', 'text');
+
+    isingModelCode.addParagraph('./resources/models/Ising/SourceCode.txt', 'code');
 
 
     document.querySelector('#Ising').addEventListener('click', () => { setModelView(isingModel); })
     
     /* 5. Crystalization */
-    var crystalizationModel = new Model('Crystalization');
-    var crystalizationModelAbout = new Chapter(crystalizationModel, 'About the Model');
-    var crystalizationModelCode = new Chapter(crystalizationModel, 'Source Code');
+    var crystalizationModel = new Model('מודל קריסטליזציה');
+    var crystalizationModelAbout = new Chapter(crystalizationModel, 'אודות המודל', 'rtl');
+    var crystalizationModelCode = new Chapter(crystalizationModel, 'קוד המודל', 'ltr');
 
-    crystalizationModelAbout.addImage('./images/models/CrystalizationBasic/CrystalizationBasic_0.png', 'Crystalization - Initial State');
-    crystalizationModelAbout.addImage('./images/models/CrystalizationBasic/CrystalizationBasic_1.png', 'Crystalization');
+    crystalizationModelAbout.addImage('./resources/models/CrystalizationBasic/CrystalizationBasic_0.png', 'בקרי סוניפיקציה');
+    crystalizationModelAbout.addImage('./resources/models/CrystalizationBasic/CrystalizationBasic_1.png', 'מצב התחלתי');
+    crystalizationModelAbout.addImage('./resources/models/CrystalizationBasic/CrystalizationBasic_2.png', 'מודל קריסטליזציה');
 
-    crystalizationModelCode.addParagraph('./images/models/CrystalizationBasic/SourceCode.txt');
+    crystalizationModelAbout.addParagraph('./resources/models/CrystalizationBasic/About.html', 'text');
+
+    crystalizationModelCode.addParagraph('./resources/models/CrystalizationBasic/SourceCode.txt', 'code');
 
     document.querySelector('#Crystalization').addEventListener('click', () => { setModelView(crystalizationModel); })
     
     /* 6. Simple Kinetics 1 */
-    var kinetics1Model = new Model('Simple Kinetics 1');
-    var kinetics1ModelAbout = new Chapter(kinetics1Model, 'About the Model');
-    var kinetics1ModelCode = new Chapter(kinetics1Model, 'Source Code');
+    var kinetics1Model = new Model('מודל Simple Kinetics 1');
+    var kinetics1ModelAbout = new Chapter(kinetics1Model, 'אודות המודל', 'rtl');
+    var kinetics1ModelCode = new Chapter(kinetics1Model, 'קוד המודל', 'ltr');
 
-    kinetics1ModelAbout.addImage('./images/models/SimpleKinetics1/SimpleKinetics1_0.png', 'Simple Kinetics 1 - Initial State');
-    kinetics1ModelAbout.addImage('./images/models/SimpleKinetics1/SimpleKinetics1_1.png', 'Simple Kinetics 1');
+    kinetics1ModelAbout.addImage('./resources/models/SimpleKinetics1/SimpleKinetics1_0.png', 'בקרי סוניפיקציה');
+    kinetics1ModelAbout.addImage('./resources/models/SimpleKinetics1/SimpleKinetics1_1.png', 'מצב התחלתי');
+    kinetics1ModelAbout.addImage('./resources/models/SimpleKinetics1/SimpleKinetics1_2.png', 'מודל Simple Kinetics 1');
 
-    kinetics1ModelCode.addParagraph('./images/models/SimpleKinetics1/SourceCode.txt');
+    kinetics1ModelAbout.addParagraph('./resources/models/SimpleKinetics1/About.html', 'text');
+
+    kinetics1ModelCode.addParagraph('./resources/models/SimpleKinetics1/SourceCode.txt', 'code');
 
     document.querySelector('#SimpleKinetics1').addEventListener('click', () => { setModelView(kinetics1Model); })
     
     /* 7. Simple Kinetics 2 */
-    var kinetics2Model = new Model('Simple Kinetics 2');
-    var kinetics2ModelAbout = new Chapter(kinetics2Model, 'About the Model');
-    var kinetics2ModelCode = new Chapter(kinetics2Model, 'Source Code');
+    var kinetics2Model = new Model('מודל Simple Kinetics 2');
+    var kinetics2ModelAbout = new Chapter(kinetics2Model, 'אודות המודל', 'rtl');
+    var kinetics2ModelCode = new Chapter(kinetics2Model, 'קוד המודל', 'ltr');
 
-    kinetics2ModelAbout.addImage('./images/models/SimpleKinetics2/SimpleKinetics2_0.png', 'Simple Kinetics 2 - Initial State');
-    kinetics2ModelAbout.addImage('./images/models/SimpleKinetics2/SimpleKinetics2_1.png', 'Simple Kinetics 2');
+    kinetics2ModelAbout.addImage('./resources/models/SimpleKinetics2/SimpleKinetics2_0.png', 'בקרי סוניפיקציה');
+    kinetics2ModelAbout.addImage('./resources/models/SimpleKinetics2/SimpleKinetics2_1.png', 'מצב התחלתי');
+    kinetics2ModelAbout.addImage('./resources/models/SimpleKinetics2/SimpleKinetics2_2.png', 'מודל Simple Kinetics 2');
 
-    kinetics2ModelCode.addParagraph('./images/models/SimpleKinetics2/SourceCode.txt');
+    kinetics2ModelAbout.addParagraph('./resources/models/SimpleKinetics2/About.html', 'text');
+
+    kinetics2ModelCode.addParagraph('./resources/models/SimpleKinetics2/SourceCode.txt', 'code');
 
     document.querySelector('#SimpleKinetics2').addEventListener('click', () => { setModelView(kinetics2Model); })
     
     /* 8. Simple Kinetics 3 */
-    var kinetics3Model = new Model('Simple Kinetics 3');
-    var kinetics3ModelAbout = new Chapter(kinetics3Model, 'About the Model');
-    var kinetics3ModelCode = new Chapter(kinetics3Model, 'Source Code');
+    var kinetics3Model = new Model('מודל Simple Kinetics 3');
+    var kinetics3ModelAbout = new Chapter(kinetics3Model, 'אודות המודל', 'rtl');
+    var kinetics3ModelCode = new Chapter(kinetics3Model, 'קוד המודל', 'ltr');
 
-    kinetics3ModelAbout.addImage('./images/models/SimpleKinetics3/SimpleKinetics3_0.png', 'Simple Kinetics 3 - Initial State');
-    kinetics3ModelAbout.addImage('./images/models/SimpleKinetics3/SimpleKinetics3_1.png', 'Simple Kinetics 3');
+    kinetics3ModelAbout.addImage('./resources/models/SimpleKinetics3/SimpleKinetics3_0.png', 'בקרי סוניפיקציה');
+    kinetics3ModelAbout.addImage('./resources/models/SimpleKinetics3/SimpleKinetics3_1.png', 'מצב התחלתי');
+    kinetics3ModelAbout.addImage('./resources/models/SimpleKinetics3/SimpleKinetics3_2.png', 'מודל Simple Kinetics 3');
 
-    kinetics3ModelCode.addParagraph('./images/models/SimpleKinetics3/SourceCode.txt');
+    kinetics3ModelAbout.addParagraph('./resources/models/SimpleKinetics3/About.html', 'text');
+
+    kinetics3ModelCode.addParagraph('./resources/models/SimpleKinetics3/SourceCode.txt', 'code');
 
     document.querySelector('#SimpleKinetics3').addEventListener('click', () => { setModelView(kinetics3Model); })
     
     /* 9. Fire */
-    var fireModel = new Model('Fire');
-    var fireModelAbout = new Chapter(fireModel, 'About the Model');
-    var fireModelCode = new Chapter(fireModel, 'Source Code');
+    var fireModel = new Model('מודל האש');
+    var fireModelAbout = new Chapter(fireModel, 'אודות המודל', 'rtl');
+    var fireModelCode = new Chapter(fireModel, 'קוד המודל', 'ltr');
 
-    fireModelAbout.addImage('./images/models/Fire/Fire_0.png', 'Fire - Initial State');
-    fireModelAbout.addImage('./images/models/Fire/Fire_1.png', 'Fire');
+    fireModelAbout.addImage('./resources/models/Fire/Fire_0.png', 'בקרי סוניפיקציה');
+    fireModelAbout.addImage('./resources/models/Fire/Fire_1.png', 'מצב התחלתי');
+    fireModelAbout.addImage('./resources/models/Fire/Fire_2.png', 'מודל האש');
+
+    fireModelAbout.addParagraph('./resources/models/Fire/About.html', 'type');
+
+    fireModelCode.addParagraph('./resources/models/Fire/SourceCode.txt', 'code');
 
     document.querySelector('#Fire').addEventListener('click', () => { setModelView(fireModel); })
     
     /* 10. Wolf Sheep Predation */
-    var wolfSheepModel = new Model('Wolf Sheep Predation');
-    var wolfSheepModelAbout = new Chapter(wolfSheepModel, 'About the Model');
-    var wolfSheepModelCode = new Chapter(wolfSheepModel, 'Source Code');
+    var wolfSheepModel = new Model('מודל Wolf Sheep Predation');
+    var wolfSheepModelAbout = new Chapter(wolfSheepModel, 'אודות המודל', 'rtl');
+    var wolfSheepModelCode = new Chapter(wolfSheepModel, 'קוד המודל', 'ltr');
 
-    wolfSheepModelAbout.addImage('./images/models/WolfSheepPredation/WolfSheepPredation_0.png', 'Wolf Sheep Predation - Initial State');
-    wolfSheepModelAbout.addImage('./images/models/WolfSheepPredation/WolfSheepPredation_1.png', 'Wolf Sheep Predation');
+    wolfSheepModelAbout.addImage('./resources/models/WolfSheepPredation/WolfSheepPredation_0.png', 'בקרי סוניפיקציה');
+    wolfSheepModelAbout.addImage('./resources/models/WolfSheepPredation/WolfSheepPredation_1.png', 'מצב התחלתי');
+    wolfSheepModelAbout.addImage('./resources/models/WolfSheepPredation/WolfSheepPredation_2.png', 'מודל Wolf Sheep Predation');
+
+    wolfSheepModelAbout.addParagraph('./resources/models/WolfSheepPredation/About.html', 'text');
+
+    wolfSheepModelCode.addParagraph('./resources/models/WolfSheepPredation/SourceCode.txt', 'code');
 
     document.querySelector('#WolfSheepPredation').addEventListener('click', () => { setModelView(wolfSheepModel); })
     
